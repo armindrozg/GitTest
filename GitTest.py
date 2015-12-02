f6e90db5253d6f27c21ea74ee16a4acd8fef3454
Armin <armin.drozg@yahoo.com>
2015-12-02 10:03:28 +0100

    Add program for computer speed test

--- GitTest.py
+++ GitTest.py
@@ -1,2 +1,14 @@
-# Test GitHub
-test = 123
+# Computer speed test
+
+import numpy as np
+import time
+
+n = 100
+
+tic = time.clock()
+for i in range(n):
+    a = np.random.rand(1000, 1000)
+
+    ainv = np.linalg.inv(a)
+toc = time.clock()
+print('Time: ', (toc-tic)/n, ' s')
